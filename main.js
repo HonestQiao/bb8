ce3edb974a2cbd869ce97618a528c9b84bfcbb8d
HonestQiao <honestqiao@163.com>
2022-05-10 10:43:46 +0800

    add key run, add output info

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
 	'use strict';
 
@@ -7,7 +7,14 @@
 		location.protocol != 'https:'
 	) {
 		location.protocol = 'https:';
-  }
+	}
+
+	const isMobile = function () {
+		const nav = navigator.userAgent.toLowerCase();
+		return (
+			nav.match(/iphone/i) || nav.match(/ipod/i) || nav.match(/ipad/i) || nav.match(/android/i)
+		);
+	};
 
 	const elConnect = document.querySelector('#connect');
 	const elStop = document.querySelector('#stop');
@@ -17,16 +24,20 @@
 	const elGreen = document.querySelector('#green');
 	const elOff = document.querySelector('#off');
 	const elJoypad = document.querySelector('#joypad');
+	const elMessage = document.querySelector('#message');
 
-	if (navigator.vibrate) {
-		[
-			elConnect, elStop, elAim, elRed, elBlue,
-			elGreen, elOff
-		].forEach(function(element) {
-			element.addEventListener('touchstart', function(event) {
-				navigator.vibrate(15);
+	const setVibrate = function() {
+		if (!isvibrate && navigator.vibrate) {
+			isvibrate = true;
+			[
+				elConnect, elStop, elAim, elRed, elBlue,
+				elGreen, elOff
+			].forEach(function (element) {
+				element.addEventListener('touchstart', function (event) {
+					navigator.vibrate(15);
+				});
 			});
-		});
+		}
 	}
 
 	const state = {
@@ -40,8 +51,12 @@
 	let gattServer;
 	let robotService;
 	let radioService;
+	let isvibrate = false;
+	let bluetoothDevice = null;
 
-	const setHeading = function(heading) {
+	// setVibrate();
+
+	const setHeading = function (heading) {
 		if (state.busy) {
 			// Return if another operation pending
 			return Promise.resolve();
@@ -54,13 +69,13 @@
 		sendCommand(did, cid, data).then(() => {
 			state.busy = false;
 		})
-		.catch(exception => {
-			console.log(exception);
-		});
+			.catch(exception => {
+				console.log(exception);
+			});
 	};
 
 	// Code based on https://github.com/WebBluetoothCG/demos/blob/gh-pages/bluetooth-toy-bb8/index.html
-	const roll = function(heading, speed, rollState) {
+	const roll = function (heading, speed, rollState) {
 		console.log('Roll heading=' + heading + ', speed=' + speed);
 		if (state.busy) {
 			// Return if another operation pending
@@ -75,12 +90,12 @@
 		sendCommand(did, cid, data).then(() => {
 			state.busy = false;
 		})
-		.catch(exception => {
-			console.log(exception);
-		});
+			.catch(exception => {
+				console.log(exception);
+			});
 	};
 
-	const stopRolling = function() {
+	const stopRolling = function () {
 		if (state.busy) {
 			setTimeout(stopRolling, 100);
 			// Return if another operation pending
@@ -95,13 +110,17 @@
 		]);
 		sendCommand(did, cid, data).then(() => {
 			state.busy = false;
+			// bluetoothDevice = null;
+			// elMessage.innerHTML = "Stop ok.";
 		})
-		.catch(exception => {
-			console.log(exception);
-		});
+			.catch(exception => {
+				bluetoothDevice = null;
+				// elMessage.innerHTML = "Stop fail.";
+				console.log(exception);
+			});
 	};
 
-	const setBackLed = function(brightness) {
+	const setBackLed = function (brightness) {
 		console.log('Set back led to ' + brightness);
 		const did = 0x02; // Virtual device ID
 		const cid = 0x21; // Set RGB LED Output command
@@ -111,8 +130,8 @@
 	};
 
 	// Code based on https://github.com/WebBluetoothCG/demos/blob/gh-pages/bluetooth-toy-bb8/index.html
-	const setColor = function(r, g, b) {
-		console.log('Set color: r='+r+',g='+g+',b='+b);
+	const setColor = function (r, g, b) {
+		console.log('Set color: r=' + r + ',g=' + g + ',b=' + b);
 		if (state.busy) {
 			// Return if another operation pending
 			return Promise.resolve();
@@ -125,13 +144,13 @@
 		sendCommand(did, cid, data).then(() => {
 			state.busy = false;
 		})
-		.catch(exception => {
-			console.log(exception);
-		});
+			.catch(exception => {
+				console.log(exception);
+			});
 	};
 
 	// Code based on https://github.com/WebBluetoothCG/demos/blob/gh-pages/bluetooth-toy-bb8/index.html
-	const sendCommand = function(did, cid, data) {
+	const sendCommand = function (did, cid, data) {
 		// Create client command packets
 		// API docs: https://github.com/orbotix/DeveloperResources/blob/master/docs/Sphero_API_1.50.pdf
 		// Next sequence number
@@ -170,6 +189,7 @@
 		}
 
 		console.log('Requesting BB-8…');
+		elMessage.innerHTML = "Connecting...";
 
 		const serviceA = '22bb746f-2bb0-7554-2d6f-726568705327';
 		const serviceB = '22bb746f-2ba0-7554-2d6f-726568705327';
@@ -184,82 +204,118 @@
 				serviceB
 			]
 		})
-		.then(device => {
-			console.log('Got device: ' + device.name);
-			return device.gatt.connect();
-		})
-		.then(server => {
-			console.log('Got server');
-			gattServer = server;
-			return gattServer.getPrimaryService(serviceA);
-		})
-		.then(service => {
-			console.log('Got service');
-			// Developer mode sequence is sent to the radio service
-			radioService = service;
-			// Get Anti DOS characteristic
-			return radioService.getCharacteristic(antiDosCharacteristicId);
-		})
-		.then(characteristic => {
-			console.log('> Found Anti DOS characteristic');
-			// Send special string
-			let bytes = new Uint8Array('011i3'.split('').map(c => c.charCodeAt()));
-			return characteristic.writeValue(bytes).then(() => {
-				console.log('Anti DOS write done.');
+			.then(device => {
+				console.log('Got device: ' + device.name);
+				bluetoothDevice = device;
+				elMessage.innerHTML = 'Got device: ' + device.name;
+				device.addEventListener('gattserverdisconnected', onDisconnected);
+				return device.gatt.connect();
 			})
-		})
-		.then(() => {
-			// Get TX Power characteristic
-			return radioService.getCharacteristic(txPowerCharacteristicId);
-		})
-		.then(characteristic => {
-			console.log('> Found TX Power characteristic');
-			const array = new Uint8Array([0x07]);
-			return characteristic.writeValue(array).then(() => {
-				console.log('TX Power write done.');
+			.then(server => {
+				console.log('Got server');
+				gattServer = server;
+				elMessage.innerHTML = 'Got server';
+				return gattServer.getPrimaryService(serviceA);
 			})
-		})
-		.then(() => {
-			// Get Wake CPU characteristic
-			return radioService.getCharacteristic(wakeCpuCharacteristicId);
-		})
-		.then(characteristic => {
-			console.log('> Found Wake CPU characteristic');
-			const array = new Uint8Array([0x01]);
-			return characteristic.writeValue(array).then(() => {
-				console.log('Wake CPU write done.');
+			.then(service => {
+				console.log('Got service');
+				// Developer mode sequence is sent to the radio service
+				radioService = service;
+				// Get Anti DOS characteristic
+				elMessage.innerHTML = 'Got service';
+				return radioService.getCharacteristic(antiDosCharacteristicId);
 			})
-		})
-		.then(() => {
-			// Get robot service
-			return gattServer.getPrimaryService(serviceB)
-		})
-		.then(service => {
-			// Commands are sent to the robot service
-			robotService = service;
-			// Get Control characteristic
-			return robotService.getCharacteristic(controlCharacteristicId);
-		})
-		.then(characteristic => {
-			console.log('> Found Control characteristic');
-			// Cache the characteristic
-			controlCharacteristic = characteristic;
-			return setColor(0, 250, 0);
-		})
-		.catch(exception => {
-			console.log(exception);
-		});
+			.then(characteristic => {
+				console.log('> Found Anti DOS characteristic');
+				elMessage.innerHTML = 'Found Anti DOS characteristic';
+				// Send special string
+				let bytes = new Uint8Array('011i3'.split('').map(c => c.charCodeAt()));
+				return characteristic.writeValue(bytes).then(() => {
+					console.log('Anti DOS write done.');
+					elMessage.innerHTML = 'Anti DOS write done.';
+				})
+			})
+			.then(() => {
+				// Get TX Power characteristic
+				elMessage.innerHTML = 'Get TX Power characteristic';
+				return radioService.getCharacteristic(txPowerCharacteristicId);
+			})
+			.then(characteristic => {
+				console.log('> Found TX Power characteristic');
+				elMessage.innerHTML = 'Found TX Power characteristic';
+				const array = new Uint8Array([0x07]);
+				return characteristic.writeValue(array).then(() => {
+					console.log('TX Power write done.');
+					elMessage.innerHTML = 'TX Power write done';
+				})
+			})
+			.then(() => {
+				// Get Wake CPU characteristic
+				elMessage.innerHTML = 'Get Wake CPU characteristic';
+				return radioService.getCharacteristic(wakeCpuCharacteristicId);
+			})
+			.then(characteristic => {
+				console.log('> Found Wake CPU characteristic');
+				elMessage.innerHTML = 'Found Wake CPU characteristic';
+				const array = new Uint8Array([0x01]);
+				return characteristic.writeValue(array).then(() => {
+					console.log('Wake CPU write done.');
+				})
+			})
+			.then(() => {
+				// Get robot service
+				elMessage.innerHTML = 'Get robot service';
+				return gattServer.getPrimaryService(serviceB)
+			})
+			.then(service => {
+				// Commands are sent to the robot service
+				robotService = service;
+				// Get Control characteristic
+				elMessage.innerHTML = 'Get Control characteristic';
+				return robotService.getCharacteristic(controlCharacteristicId);
+			})
+			.then(characteristic => {
+				console.log('> Found Control characteristic');
+				elMessage.innerHTML = 'Found Control characteristic';
+				// Cache the characteristic
+				controlCharacteristic = characteristic;
+				// alert('connect ok');
+				elMessage.innerHTML = "Connected to " + bluetoothDevice.name;
+				elConnect.innerHTML = "Connected";
+				return setColor(0, 250, 0);
+			})
+			.catch(exception => {
+				bluetoothDevice = null;
+				elMessage.innerHTML = "Connect fail";
+				elConnect.innerHTML = "Connect";
+				console.log(exception);
+			});
 	};
 
-	elConnect.onclick = function() {
-		connect();
+	elConnect.onclick = function () {
+		if(bluetoothDevice && bluetoothDevice.gatt.connected) {
+			elMessage.innerHTML = "Connect ok";
+		} else {
+			connect();
+		}
 	};
 
-	elStop.onclick = function() {
-		stopRolling();
+	elStop.onclick = function () {
+		elMessage.innerHTML = "Stoping...";
+		if(bluetoothDevice && bluetoothDevice.gatt.connected) {
+			stopRolling();
+			setTimeout(()=>{
+				bluetoothDevice.gatt.disconnect();
+				elMessage.innerHTML = "Stop OK";
+				elConnect.innerHTML = "Connect";
+			}, 500);
+		} else {
+			elMessage.innerHTML = "Stop OK";
+			elConnect.innerHTML = "Connect";
+		}
 	};
 
-	elAim.onclick = function() {
+	elAim.onclick = function () {
 		state.aim = !state.aim;
 		if (state.aim) {
 			setBackLed(0xff).then(() => setColor(0, 0, 0));
@@ -269,65 +325,81 @@
 		elAim.classList.toggle('active');
 	};
 
-	elRed.onclick = function() {
+	elRed.onclick = function () {
 		setColor(255, 0, 0);
 	};
 
-	elGreen.onclick = function() {
+	elGreen.onclick = function () {
 		setColor(0, 255, 0);
 	};
 
-	elBlue.onclick = function() {
+	elBlue.onclick = function () {
 		setColor(0, 0, 255);
 	};
 
-	elOff.onclick = function() {
+	elOff.onclick = function () {
 		setColor(0, 0, 0);
 	};
 
 	const radius = 150;
 
-	const handleTouchEvent = function(event) {
+	const onDisconnected = function(event) {
+		if(bluetoothDevice) {
+			const device = event.target;
+			console.log(`Device ${device.name} is disconnected.`);
+			elMessage.innerHTML = `Device ${device.name} is disconnected.`;
+			bluetoothDevice = null;
+		}
+	}
+
+	const runCtrl = function(x, y){
+		// Notes: x and y are swapped here in order to get clockwise theta from Y-axis.
+		const theta = Math.PI - Math.atan2(x - radius, y - radius);
+		const degrees = theta * (180 / Math.PI);
+		const tx = Math.abs(x - radius);
+		const ty = Math.abs(y - radius);
+		let speed = Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2));
+		speed = speed / 150.0 * 255.0;
+		console.log('event: ' + x + ', ' + y + ', d: ' + degrees + ' speed: ' + speed);
+		if (state.aim) {
+			roll(Math.round(degrees), 0, 1);
+		} else {
+			roll(Math.round(degrees), Math.round(speed), 1);
+		}
+	}
+
+	const handleTouchEvent = function (event) {
 		event.preventDefault();
 		if (event.targetTouches.length == 1) {
 			const touch = event.targetTouches[0];
 			const x = touch.clientX - elJoypad.offsetLeft;
 			const y = touch.pageY - elJoypad.offsetTop;
 			// Notes: x and y are swapped here in order to get clockwise theta from Y-axis.
-			const theta = Math.PI - Math.atan2(x - radius, y - radius);
-			const degrees = theta * (180 / Math.PI);
-			const tx = Math.abs(x - radius);
-			const ty = Math.abs(y - radius);
-			let speed = Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2));
-			speed = speed / 150.0 * 255.0;
-			console.log('event: ' + x + ', ' + y + ', d: ' + degrees + ' speed: ' + speed);
-			if (state.aim) {
-				roll(Math.round(degrees), 0, 1);
-			} else {
-				roll(Math.round(degrees), Math.round(speed), 1);
-			}
+			runCtrl(x, y);
 		}
 	};
 
-	elJoypad.ontouchstart = function(event) {
+	elJoypad.ontouchstart = function (event) {
 		handleTouchEvent(event);
 	};
 
-	elJoypad.ontouchmove = function(event) {
+	elJoypad.ontouchmove = function (event) {
+		setVibrate();
 		handleTouchEvent(event);
 	}
 
-	elJoypad.ontouchend = function(event) {
+	elJoypad.ontouchend = function (event) {
 		event.preventDefault();
 		stopRolling();
 	};
 
 	var keyName = "";
 	var keystring = "";//记录按键的字符串
+	var keyDirs = [];
 	function keyboard(eve) {
-        let key = eve.key;
-        let keyCode = eve.keyCode || eve.which;
-        console.log(`key=${key} keyCode=${keyCode}`);
+		let key = eve.key;
+		let keyCode = eve.keyCode || eve.which;
+		console.log(`key=${key} keyCode=${keyCode}`);
 	}
 
 	function keypress(e) {
@@ -336,7 +408,6 @@
 		CapsLock = currKey >= 65 && currKey <= 90;
 		switch (currKey) {
 			//屏蔽了退格、制表、回车、空格、方向键、删除键
-			//Exclude: Backspace, Tab, Return, Direction Key, Delete
 			case 8:
 			case 9:
 			case 13:
@@ -377,12 +448,20 @@
 				case 36: keyName = "[Home]";
 					break;
 				case 37: keyName = "[Left]";
+					// runCtrl(radius-20,radius);
+					keyDirs.push('left');
 					break;
 				case 38: keyName = "[Up]";
+					// runCtrl(radius,radius-20);
+					keyDirs.push('up');
 					break;
 				case 39: keyName = "[Right]";
+					// runCtrl(radius+20,radius);
+					keyDirs.push('right');
 					break;
 				case 40: keyName = "[Down]";
+					// runCtrl(radius,radius+20);
+					keyDirs.push('down');
 					break;
 				case 46: keyName = "[Delete]";
 					break;
@@ -391,16 +470,42 @@
 			}
 			keystring += keyName;
 		}
-		console.log("keydown:"+keyName);
+		console.log("keydown:" + keyName);
 	}
 	function keyup(e) {
-		console.log("keyup:"+keystring);
+		console.log("keyup:" + keystring);
+		let x = radius;
+		let y = radius;
+		let prev_dir = '';
+		for(let i=0;i<keyDirs.length;i++) {
+			switch (dir) {
+				case 'left':
+					x -= dir == prev_dir ? 2 : 1;
+					break;
+				case 'right':
+					x += dir == prev_dir ? 2 : 1;
+					break;
+				case 'up':
+					y -= dir == prev_dir ? 2 : 1;
+					break;
+				case 'down':
+					y += dir == prev_dir ? 2 : 1;
+					break;
+				default:
+					break;
+			}
+			prev_dir = dir;
+		}
+		x = Math.max(Math.min(x, radius), 0);
+		y = Math.max(Math.min(y, radius), 0);
+		runCtrl(x, y);
 		keystring = "";
+		keyDirs = [];
 	}
-	document.addEventListener('keypress', keypress);
+	// document.addEventListener('keypress', keypress);
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
-	document.onkeypress = keypress;
+	// document.onkeypress = keypress;
 	document.onkeydown = keydown;
-	document.onkeyup =keyup;
+	document.onkeyup = keyup;
 }());
